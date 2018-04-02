197cc1652de174eb6f3fcde567d70a0de72a556a
Christopher H Roper <roperch@optusnet.com.au>
2018-04-02 13:34:37 +1000

    Is he gonna survive?
    A hero is on his way to the castle to complete his mission. However, he's been told that the castle is surrounded with a couple of powerful dragons! each dragon takes 2 bullets to be defeated, our hero has no idea how many bullets he should carry.. Assuming he's gonna grab a specific given number of bullets and move forward to fight another specific given number of dragons, will he survive?
    
    Return True if yes, False otherwise :)

---
+++ is-he-gonna-survive.js
@@ -0,0 +1,13 @@
+# test cases:
+# hero(10, 5) --> true
+# hero(7, 4) --> false
+# hero(4, 5) --> false
+# hero(100, 40) true
+# hero(1500, 751) --> false
+# hero(0, 1) --> false
+
+function hero(bullets, dragons){
+ if (bullets/2 >= dragons)
+   return true;
+  return false;
+}

6e7bf1a9e0453ee269baf01efd3ca0e5f7c7d214
Christopher H Roper <roperch@optusnet.com.au>
2018-04-07 18:50:00 +1000

    Training JS #7: if..else and ternary operator
    Complete function saleHotdogs/SaleHotDogs, function accept 1 parameters:n, n is the number of customers to buy hotdogs, different numbers have different prices (refer to the following table), return a number that the customer need to pay how much money.
    
    +---------------+-------------+
    |  numbers n    | price(cents)|
    +---------------+-------------+
    |n<5            |    100      |
    +---------------+-------------+
    |n>=5 and n<10  |     95      |
    +---------------+-------------+
    |n>=10          |     90      |
    +---------------+-------------+
    You can use if..else or ternary operator to complete it.
    
    When you have finished the work, click "Run Tests" to see if your code is working properly.
    
    In the end, click "Submit" to submit your code pass this kata.

---
+++ if-else-ternary.js
@@ -0,0 +1,16 @@
+// test cases:
+// saleHotdogs(1) --> 100
+// saleHotdogs(4) --> 400
+// saleHotdogs(5) --> 475
+// saleHotdogs(9) --> 855
+// saleHotdogs(10) --> 900
+// saleHotdogs(100) --> 9000
+
+function saleHotdogs(n){
+  if (n<5)
+    return n*100;
+  else if (n>=5 && n<10)
+    return n*95;
+  else if (n>=10)
+    return n*90
+}

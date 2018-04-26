23fda6dbb683d7f8eef904df48fa7fd7cfcd80c2
Christopher H Roper <roperch@optusnet.com.au>
2018-04-26 18:40:25 +1000

    Printing Array elements with Comma delimiters
    Input: Array of elements
    
    ["h","o","l","a"]
    
    Output: String with comma delimited elements of the array in th same order.
    
    "h,o,l,a"

---
+++ arrays-with-commas.js
@@ -0,0 +1,9 @@
+// test cases:
+//printArray([2,3,6,5,7]) --> '23657'
+//printArray(['p','r','i','n','t','','a','r','r','a','y']) --> 'print array'
+//printArray(['h','e','l','l','o','','w','o','r','l','d']) --> 'hello world'
+
+
+function printArray(array){
+  return array.join(",");
+}
